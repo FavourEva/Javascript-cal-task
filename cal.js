6db06a9c74aa154f6d7b0d9ab3db5314ed2726cb
FavourEva <favoureva118@gmail.com>
2022-06-25 23:06:02 +0100

    javacript cal without frontend

---
+++ cal.js
@@ -0,0 +1,30 @@
+// program for a basic arithmetic calculator without a frontend
+
+//take the first operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+
+// take the operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// take the  second operand input
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+//using if...else if... else
+if (operator == '+') {
+result = number1 + number2;
+}
+else if (operator == '-') {
+result = number1 - number2;
+}
+else if (operator == '*') {
+result = number1 * number2;
+}
+else {
+result = number1 / number2;
+}
+
+// display the result
+alert(result);
+
